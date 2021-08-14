7ff4490c6d6f6f7e28a50bc067e6cef219ae8c9b
Pranav <83628215+Pranav18262@users.noreply.github.com>
2021-08-14 10:06:21 +0530

    Update Config.py

--- src/Config.py
+++ src/Config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     aid = int(os.environ.get("7941936")
-    ahash = os.environ.get("6db0cab0f0a81879cd82d4fff6b2f608
+    ahash = os.environ.get("6db0cab0f0a81879cd82d4fff6b2f608")
 ")
     bot_token = os.environ.get("1846833063:AAH-v4vgsai6dD_akKEhUvR6wjOp86w9MPM")
     sudo = [239508098, 1313665327]
